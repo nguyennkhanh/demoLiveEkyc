d50fe4a0948432592baf26f806b30a5d69629e54
khanhnv <ctg.nguyenkhanh@gmail.com>
2023-07-13 11:55:35 +0700

    commit

---
+++ sdk_liveness_rn_demo/ios/HeadPoseFramework.xcframework/ios-x86_64-simulator/HeadPoseFramework.framework/Headers/HeadPoseFramework.h
@@ -0,0 +1,19 @@
+//
+//  HeadPoseFramework.h
+//  HeadPoseFramework
+//
+//  Created by Hungnx on 10/21/20.
+//
+
+#import <Foundation/Foundation.h>
+#import "NCNNWrapper.h"
+
+//! Project version number for HeadPoseFramework.
+FOUNDATION_EXPORT double HeadPoseFrameworkVersionNumber;
+
+//! Project version string for HeadPoseFramework.
+FOUNDATION_EXPORT const unsigned char HeadPoseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeadPoseFramework/PublicHeader.h>
+
+
